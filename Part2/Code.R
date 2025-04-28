c9940f4aa4b9456a6368623647d1f083eb6fdf9b
Esmaeil Roohparvar Basmenj <e.roohparvar@modares.ac.ir>
2025-04-28 12:54:35 +1000

    Corrected and updated sample data files

--- Part2/Code.R
+++ Part2/Code.R
@@ -1,18 +1,13 @@
 ################################################################################ Start library
 library(Seurat)
 library(SeuratObject)
-
-
-if (!requireNamespace("BiocManager", quietly=TRUE)) {
-  install.packages("BiocManager")
-}
-BiocManager::install("zellkonverter")
-library(zellkonverter)
 ################################################################################ End library
 
 
 
 ################################################################################ Start CD4
-file_path <- "C:/Esmaeil/scRNA-seq/Single-Cell-Pipeline-in-R/Part2/Data/GSE206299_ircolitis-tissue-cd4.h5ad"
-h5ad <- system.file("extdata", file_path ,package = "zellkonverter")
-adata_CD4 = readH5AD(h5ad)
+h5ad_file <- "C:/Esmaeil/scRNA-seq/Single-Cell-Pipeline-in-R/Part2/Data/GSE206299_ircolitis-tissue-cd4.h5ad"
+adata <- read_h5ad("C:/Esmaeil/scRNA-seq/Single-Cell-Pipeline-in-R/Part2/Data/GSE206299_ircolitis-tissue-cd4.h5ad")
+mtx = adata$X
+meta = adata$obs
+genes = adata$var_names
