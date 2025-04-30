ac037e7d5105705d7f890ca00c821e1b34e18867
Esmaeil Roohparvar Basmenj <e.roohparvar@modares.ac.ir>
2025-04-30 12:16:02 +1000

    Performed quality control on Control - Healthy dataset

--- Part2/Code.R
+++ Part2/Code.R
@@ -266,3 +266,151 @@ dev.off()
 
 seurat_obj_7=subset(seurat_obj_7,subset=nFeature_RNA>300 & nFeature_RNA<2000 & MTpercent<20 & nCount_RNA>600 & nCount_RNA<15000)
 ################################################################################ End Control Healthy SIC_187 #7
+
+
+
+################################################################################ Start Control Healthy SIC_188_A #8
+setwd("C:/Esmaeil/scRNA-seq/Single-Cell-Pipeline-in-R/Part2/Data/5p/Control - Healthy/GSM6250081_SIC_188_Colon_585_CD45sort_5p_GEX_A")
+data_dir <- "C:/Esmaeil/scRNA-seq/Single-Cell-Pipeline-in-R/Part2/Data/5p/Control - Healthy/GSM6250081_SIC_188_Colon_585_CD45sort_5p_GEX_A/raw_feature_bc_matrix.h5"
+
+
+raw_data <- Read10X_h5(data_dir)
+seurat_obj_8 <- CreateSeuratObject(counts = raw_data, project = "Control Healthy SIC_188_A",min.cells=3,min.features=200)
+seurat_obj_8[["MTpercent"]]=PercentageFeatureSet(seurat_obj_8,pattern = "^MT-")
+
+
+png(filename = "1.png",width = 10000,height=4000,units ="px",res = 600)
+ggplot(data = seurat_obj_8@meta.data, aes(x = seurat_obj_8$nCount_RNA, y = seurat_obj_8$MTpercent)) +
+  geom_point(size = 2, color = 'blue') +
+  labs(x = 'nCount_RNA', y = 'perc. mito') +
+  scale_x_log10() +
+  geom_hline(yintercept = 20, color = 'red') + 
+  geom_vline(xintercept = 800, color = 'red') +
+  geom_vline(xintercept = 15000, color = 'red')
+dev.off()
+
+
+png(filename = "2.png",width = 10000,height=4000,units ="px",res = 600)
+ggplot(data=seurat_obj_8@meta.data, aes(x=seurat_obj_8$nFeature_RNA, y=seurat_obj_8$MTpercent)) +
+  geom_point(size=2, color = 'blue') +
+  labs(x='nFeature_RNA', y='perc. mito') +
+  scale_x_log10() +
+  geom_hline(yintercept=20, color='red') + 
+  geom_vline(xintercept=400, color='red') +
+  geom_vline(xintercept=2000, color='red') 
+dev.off()
+
+
+seurat_obj_8=subset(seurat_obj_8,subset=nFeature_RNA>400 & nFeature_RNA<2000 & MTpercent<20 & nCount_RNA>800 & nCount_RNA<15000)
+################################################################################ End Control Healthy SIC_188_A #8
+
+
+
+################################################################################ Start Control Healthy SIC_188_B #9
+setwd("C:/Esmaeil/scRNA-seq/Single-Cell-Pipeline-in-R/Part2/Data/5p/Control - Healthy/GSM6250082_SIC_188_Colon_585_CD45sort_5p_GEX_B")
+data_dir <- "C:/Esmaeil/scRNA-seq/Single-Cell-Pipeline-in-R/Part2/Data/5p/Control - Healthy/GSM6250082_SIC_188_Colon_585_CD45sort_5p_GEX_B/raw_feature_bc_matrix.h5"
+
+
+raw_data <- Read10X_h5(data_dir)
+seurat_obj_9 <- CreateSeuratObject(counts = raw_data, project = "Control Healthy SIC_188_B",min.cells=3,min.features=200)
+seurat_obj_9[["MTpercent"]]=PercentageFeatureSet(seurat_obj_9,pattern = "^MT-")
+
+
+png(filename = "1.png",width = 10000,height=4000,units ="px",res = 600)
+ggplot(data = seurat_obj_9@meta.data, aes(x = seurat_obj_9$nCount_RNA, y = seurat_obj_9$MTpercent)) +
+  geom_point(size = 2, color = 'blue') +
+  labs(x = 'nCount_RNA', y = 'perc. mito') +
+  scale_x_log10() +
+  geom_hline(yintercept = 23, color = 'red') + 
+  geom_vline(xintercept = 700, color = 'red') +
+  geom_vline(xintercept = 15000, color = 'red')
+dev.off()
+
+
+png(filename = "2.png",width = 10000,height=4000,units ="px",res = 600)
+ggplot(data=seurat_obj_9@meta.data, aes(x=seurat_obj_9$nFeature_RNA, y=seurat_obj_9$MTpercent)) +
+  geom_point(size=2, color = 'blue') +
+  labs(x='nFeature_RNA', y='perc. mito') +
+  scale_x_log10() +
+  geom_hline(yintercept=23, color='red') + 
+  geom_vline(xintercept=300, color='red') +
+  geom_vline(xintercept=2000, color='red') 
+dev.off()
+
+
+seurat_obj_9=subset(seurat_obj_9,subset=nFeature_RNA>300 & nFeature_RNA<2000 & MTpercent<23 & nCount_RNA>700 & nCount_RNA<15000)
+################################################################################ End Control Healthy SIC_188_B #9
+
+
+
+################################################################################ Start Control Healthy SIC_612_A #10
+setwd("C:/Esmaeil/scRNA-seq/Single-Cell-Pipeline-in-R/Part2/Data/5p/Control - Healthy/GSM6250083_SIC_196_Colon_612_CD45sort_5p_GEX_A")
+data_dir <- "C:/Esmaeil/scRNA-seq/Single-Cell-Pipeline-in-R/Part2/Data/5p/Control - Healthy/GSM6250083_SIC_196_Colon_612_CD45sort_5p_GEX_A/raw_feature_bc_matrix.h5"
+
+
+raw_data <- Read10X_h5(data_dir)
+seurat_obj_10 <- CreateSeuratObject(counts = raw_data, project = "Control Healthy SIC_612_A",min.cells=3,min.features=200)
+seurat_obj_10[["MTpercent"]]=PercentageFeatureSet(seurat_obj_10,pattern = "^MT-")
+
+
+png(filename = "1.png",width = 10000,height=4000,units ="px",res = 600)
+ggplot(data = seurat_obj_10@meta.data, aes(x = seurat_obj_10$nCount_RNA, y = seurat_obj_10$MTpercent)) +
+  geom_point(size = 2, color = 'blue') +
+  labs(x = 'nCount_RNA', y = 'perc. mito') +
+  scale_x_log10() +
+  geom_hline(yintercept = 25, color = 'red') + 
+  geom_vline(xintercept = 500, color = 'red') +
+  geom_vline(xintercept = 14000, color = 'red')
+dev.off()
+
+
+png(filename = "2.png",width = 10000,height=4000,units ="px",res = 600)
+ggplot(data=seurat_obj_10@meta.data, aes(x=seurat_obj_10$nFeature_RNA, y=seurat_obj_10$MTpercent)) +
+  geom_point(size=2, color = 'blue') +
+  labs(x='nFeature_RNA', y='perc. mito') +
+  scale_x_log10() +
+  geom_hline(yintercept=25, color='red') + 
+  geom_vline(xintercept=300, color='red') +
+  geom_vline(xintercept=1800, color='red') 
+dev.off()
+
+
+seurat_obj_10=subset(seurat_obj_10,subset=nFeature_RNA>300 & nFeature_RNA<1800 & MTpercent<25 & nCount_RNA>500 & nCount_RNA<14000)
+################################################################################ End Control Healthy SIC_612_A #10
+
+
+
+################################################################################ Start Control Healthy SIC_612_B #11
+setwd("C:/Esmaeil/scRNA-seq/Single-Cell-Pipeline-in-R/Part2/Data/5p/Control - Healthy/GSM6250084_SIC_196_Colon_612_CD45sort_5p_GEX_B")
+data_dir <- "C:/Esmaeil/scRNA-seq/Single-Cell-Pipeline-in-R/Part2/Data/5p/Control - Healthy/GSM6250084_SIC_196_Colon_612_CD45sort_5p_GEX_B/raw_feature_bc_matrix.h5"
+
+
+raw_data <- Read10X_h5(data_dir)
+seurat_obj_11 <- CreateSeuratObject(counts = raw_data, project = "Control Healthy SIC_612_B",min.cells=3,min.features=200)
+seurat_obj_11[["MTpercent"]]=PercentageFeatureSet(seurat_obj_11,pattern = "^MT-")
+
+
+png(filename = "1.png",width = 10000,height=4000,units ="px",res = 600)
+ggplot(data = seurat_obj_11@meta.data, aes(x = seurat_obj_11$nCount_RNA, y = seurat_obj_11$MTpercent)) +
+  geom_point(size = 2, color = 'blue') +
+  labs(x = 'nCount_RNA', y = 'perc. mito') +
+  scale_x_log10() +
+  geom_hline(yintercept = 25, color = 'red') + 
+  geom_vline(xintercept = 400, color = 'red') +
+  geom_vline(xintercept = 14000, color = 'red')
+dev.off()
+
+
+png(filename = "2.png",width = 10000,height=4000,units ="px",res = 600)
+ggplot(data=seurat_obj_11@meta.data, aes(x=seurat_obj_11$nFeature_RNA, y=seurat_obj_11$MTpercent)) +
+  geom_point(size=2, color = 'blue') +
+  labs(x='nFeature_RNA', y='perc. mito') +
+  scale_x_log10() +
+  geom_hline(yintercept=25, color='red') + 
+  geom_vline(xintercept=250, color='red') +
+  geom_vline(xintercept=1800, color='red') 
+dev.off()
+
+
+seurat_obj_11=subset(seurat_obj_11,subset=nFeature_RNA>150 & nFeature_RNA<1800 & MTpercent<25 & nCount_RNA>400 & nCount_RNA<14000)
+################################################################################ End Control Healthy SIC_612_B #11
