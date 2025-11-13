d0edc17f801da87763a16734d7aaf5956f387a70
Esmaeil Roohparvar Basmenj <e.roohparvar@modares.ac.ir>
2025-11-14 10:52:24 +1100

    Update Python code for dataset 4

--- Part 4/Python Data Preparation/Code.py
+++ Part 4/Python Data Preparation/Code.py
@@ -7,23 +7,42 @@ print("scanpy:", scanpy.__version__)
 print("pandas:", pandas.__version__)
 
 
-import anndata as ad
-import pandas as pd
 
+# Load the .h5ad file
 adata = ad.read_h5ad("C:/Esmaeil/irAEsProject/Backup/Part 4/0_Data/GSE253720_Biopsy_RNA_Final.h5ad")
+
+
 
 # Extract raw counts matrix
 expr = pd.DataFrame(adata.layers["counts"].toarray(), index=adata.obs_names, columns=adata.var_names)
-# Metadata
-obs = adata.obs
-var = adata.var
+# Check if all values are integers to confirm that this is the raw counts matrix, not normalized or log-transformed data
+has_fraction = ((expr.values % 1) != 0).any()
+print("Are there any real decimal numbers in expr (e.g., 2.6, 7.9)?", has_fraction)
 
-# Save to CSVs (or optionally RDS later)
+
+
+# For additional confirmation, we extract the default matrix (adata.X) to show that it contains decimals (non-integer values), indicating that it is normalized/log-transformed and not the raw counts matrix
+expr2 = adata.to_df()
+has_fraction = ((expr2.values % 1) != 0).any()
+print("Are there any real decimal numbers in expr2 (e.g., 2.6, 7.9)?", has_fraction)
+
+
+
+# So, based on this comparison, expr contains the raw counts that we want, while expr2 contains the normalized data. Therefore, we should save expr, the raw counts matrix.
 expr.to_csv("C:/Esmaeil/irAEsProject/Backup/Part 4/0_Data/Python Data Preparation/expression.csv")
+
+
+
+# And save Metadata
+obs = adata.obs
 obs.to_csv("C:/Esmaeil/irAEsProject/Backup/Part 4/0_Data/Python Data Preparation/metadata_obs.csv")
+
+var = adata.var
 var.to_csv("C:/Esmaeil/irAEsProject/Backup/Part 4/0_Data/Python Data Preparation/metadata_var.csv")
 
 
+
+# print summary
 print("✅ File loaded successfully!")
 print("Shape:", adata.shape)
 print("Observations (cells):", len(adata.obs))
--- Part 5/Code.R
+++ Part 5/Code.R
@@ -67,48 +67,22 @@ for (i in seq_along(rds_files4)) {
 
 
 ################################################################################ Start integration
-plan("multicore", workers = 2)
-options(future.globals.maxSize = 54 * 1024^3)
+# plan("multicore", workers = 2)
+# options(future.globals.maxSize = 54 * 1024^3)
 
 
-merged_obj <- merge(srob1, y = list(srob2, srob3, srob4, 
-                                    srob5, srob6, srob7, 
-                                    srob8, srob9, srob10, 
-                                    srob11, srob12, srob13,
-                                    srob14, srob15, srob16,
-                                    srob17, srob18, srob19,
-                                    srob20, srob21, srob22,
-                                    srob23, srob24, srob25,
-                                    srob26, srob27, srob28,
-                                    srob29, srob30, srob31,
-                                    srob32, srob32, srob33,
-                                    srob34, srob35, srob36,
-                                    srob37, srob38, srob39,
-                                    srob40, srob41, srob42,
-                                    srob43, srob44, srob45,
-                                    srob46, srob47, srob48,
-                                    srob49, srob50, srob51,
-                                    srob52, srob53, srob54,
-                                    srob55, srob56, srob57,
-                                    srob58, srob59, srob60,
-                                    srob61, srob62, srob63,
-                                    srob64, srob65, srob66,
-                                    srob67, srob68, srob69,
-                                    srob70, srob71, srob72,
-                                    srob73, srob74, srob75,
-                                    srob76, srob77, srob78,
-                                    srob79, srob80, srob81,
-                                    srob82, srob83, srob84, 
-                                    srob85, srob86, srob87,
-                                    srob88, srob89, srob90,
-                                    srob91, srob92, srob93,
-                                    srob94, srob95, srob96,
-                                    srob97))
-
+merged_obj <- merge(srob1, y = list(srob2, srob3, srob4,srob5, srob6, srob7, srob8, srob9, srob10, srob11, srob12, srob13,
+                                    srob14, srob15, srob16, srob17, srob18, srob19, srob20, srob21, srob22, srob23, srob24, srob25,
+                                    srob26, srob27, srob28, srob29, srob30, srob31, srob32, srob33, srob34, srob35, srob36, srob37,
+                                    srob38, srob39, srob40, srob41, srob42, srob43, srob44, srob45, srob46, srob47, srob48, srob49,
+                                    srob50, srob51, srob52, srob53, srob54, srob55, srob56, srob57, srob58, srob59, srob60, srob61,
+                                    srob62, srob63, srob64, srob65, srob66, srob67, srob68, srob69, srob70, srob71, srob72, srob73,
+                                    srob74, srob75, srob76, srob77, srob78, srob79, srob80, srob81, srob82, srob83, srob84, srob85,
+                                    srob86, srob87, srob88, srob89, srob90, srob91, srob92, srob93, srob94, srob95, srob96, srob97))
+gc() 
 
 rm(list = paste0("srob", 1:97))
 gc() 
-
 
 merged_obj=NormalizeData(merged_obj,normalization.method = "LogNormalize",scale.factor = 10000)
 gc()
@@ -130,15 +104,16 @@ saveRDS(file = "merged_obj",merged_obj)
 gc()
 
 merged_obj <- IntegrateLayers(object = merged_obj,
-                              method = CCAIntegration,
+                              method = JointPCAIntegration,
                               orig.reduction = "pca", 
-                              new.reduction = "integrated.cca")
+                              new.reduction = "integrated.dr",
+                              verbose = FALSE)
 
 gc()
 
 # 2
-# The Seurat object obtained after IntegrateLayers
-setwd("C:/Esmaeil/irAEsProject/Backup/Part 5/2_The Seurat object obtained after IntegrateLayers")
+# The Seurat object obtained after IntegrateLayers with JointPCAIntegration
+setwd("C:/Esmaeil/irAEsProject/Backup/Part 5/2_The Seurat object obtained after IntegrateLayers with JointPCAIntegration")
 saveRDS(file = "merged_obj",merged_obj)
 
 
